bca182f94ed189948145d9e9e863fc7b23450cb0
Mchrispin <mchrispin14@gmail.com>
2024-10-06 15:55:01 +0200

    'last-commit'

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
 package Mortgage_calculator;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mortgage calculation");
         var mortgage1 = new MortgageCalc();
-        String Kalisa_mortgage = mortgage1.getMortgage();
-        System.out.println("Kalisa's mortgage is "+ Kalisa_mortgage);
+        String mortgage = mortgage1.start();
+        System.out.println(mortgage);
     }
 }
--- MortgageCalc.java
+++ MortgageCalc.java
@@ -1,29 +1,23 @@
 package Mortgage_calculator;
-import java.nio.ByteBuffer;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class MortgageCalc {
     private int principal;
     private float monthly_interest_rate;
-    private int period;
+    private byte period;
     private final int MONTHS_IN_YEAR = 12;
     final int PERCENT = 100;
     final double interest = 0.0391;
 
     public static String Mortgage;
 
-    public String getMortgage() {
-        Scanner sc1 = new Scanner(System.in);
-        System.out.print("Principal: ");
-        setPrincipal(sc1.nextInt());
-
-        System.out.print("Annual Interest Rate: ");
-        setMonthly_interest_rate(sc1.nextFloat());
-
-        System.out.print("Period (years): ");
-        setPeriod(sc1.nextInt());
-        sc1.close();
+    Scanner sc = new Scanner(System.in);
+    public String start() {
+        this.principal = setPrincipal();
+        this.monthly_interest_rate = setMonthly_interest_rate();
+        this.period = setPeriod();
+        sc.close();
         return mortgage();
     }
 
@@ -34,21 +28,44 @@ public class MortgageCalc {
         return NumberFormat.getCurrencyInstance().format(result);
     }
 
-    private void setPrincipal(int principal) {
-        if (principal < 0)
-            throw new IllegalArgumentException("Principal must start from 0 and above");
-        this.principal = principal;
+    private int setPrincipal() {
+        while(true){
+            System.out.print("Principal ($1k - $1M): ");
+            principal = sc.nextInt();
+            if (!(principal >= 1000 && principal <= 1000_000) ) {
+                System.out.println("Enter a number between 1000 and 1000_000.");
+                continue;
+            }
+            break;
+        }
+        return principal;
     }
-    private void setPeriod(int period) {
-        if (period < 0)
-            throw new IllegalArgumentException("Period must start from 0 and above");
-        this.period = period * MONTHS_IN_YEAR;
+
+    private byte setPeriod() {
+        while(true){
+            System.out.print("Period (Years): ");
+            period = sc.nextByte();
+            if (!(period >= 1 && period  <= 30) ) {
+                System.out.println("Enter a value between 1 and 30");
+                continue;
+            }
+            break;
+        }
+        return (byte) period;
     }
-    private void setMonthly_interest_rate(float annual_interest_rate) {
-        if (annual_interest_rate < 0)
-            throw new IllegalArgumentException("Annual interest rate must start from 0 and above");
-        float monthly = annual_interest_rate / PERCENT / MONTHS_IN_YEAR;
-        this.monthly_interest_rate = monthly;
+
+
+    private float setMonthly_interest_rate() {
+        while(true){
+            System.out.print("Annual interest rate: ");
+            monthly_interest_rate = sc.nextFloat();
+            if (!(monthly_interest_rate > 0 && monthly_interest_rate  <= 30) ) {
+                System.out.println("Enter a value greater than 1 and less than or equal to 30");
+                continue;
+            }
+            break;
+        }
+        return (float) monthly_interest_rate;
     }
 
     public int getPrincipal() {
